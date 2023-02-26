02a833c46a8fa9f6c50af216f5d7bb90040b9747
Sergey <gul.19967@gmail.com>
2023-02-27 02:33:53 +0300

    add seminar4

--- Seminars/Seminar4/Program.cs
+++ Seminars/Seminar4/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+// int FindSum(int A)
+// {
+//     int sum = 0;
+//     for (int current = 1; current <= A; current++)
+//     {
+//         sum = sum + current; //sum += current
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Input A");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(FindSum(A));
+
+//Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// //int[] myArray = CreateRandomArray(size, min, max);
+// ShowArray(CreateRandomArray(size, min, max));
+
+//Задача 2.Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+// int CountNumber(int num)
+// {
+//     int count = 1;
+//     while (count < num)
+//     {
+//         num /= 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(CountNumber(num));
+
+//Задача 3. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+//4 -> 24
+//5 -> 120
+
+// int Factorial(int num)
+// {
+//     int n = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         n *= i;
+//     }
+//     return n;
+// }
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Factorial(num));
